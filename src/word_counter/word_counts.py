4bee7043aa9d7eb24f2572c9d148b8c3f4a8793f
jaymin.desai <jaymindesai766@gmail.com>
2018-12-02 17:06:52 -0500

    Word Counts

--- src/word_counter/word_counts.py
+++ src/word_counter/word_counts.py
@@ -12,7 +12,7 @@ def count_words():
     true_counts = Counter()
 
     for _, row in data.iterrows():
-        print(row)
+        # print(row)
         tokens = [t for t in tokenize(row['statement'])]
         if row['label'] in ['TRUE', 'mostly-true', 'half-true']:
             true_counts.update(tokens)
@@ -21,3 +21,24 @@ def count_words():
 
     print('True', true_counts.most_common(10))
     print('Fake', fake_counts.most_common(10))
+
+    true_words = set()
+    fake_words = set()
+
+    with open('word_freq_true.txt', 'w') as file:
+        for word, freq in true_counts.most_common():
+            true_words.add(word)
+            file.write('{}: {}\n'.format(word, freq))
+
+    with open('word_freq_fake.txt', 'w') as file:
+        for word, freq in fake_counts.most_common():
+            fake_words.add(word)
+            file.write('{}: {}\n'.format(word, freq))
+
+    with open('true_fake_diff.txt', 'w') as file:
+        for word in fake_words - true_words:
+            file.write('{}: {}\n'.format(word, fake_counts[word]))
+
+
+if __name__ == '__main__':
+    count_words()
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from collections import Counter
 
-# x = [[10, 20], [11, 21]]
+x = ['a a']
+y = ['a b']
+
+c = Counter(x)
+c.update(y)
+print(c)
+
 # y = [1, 2]
 # df = pd.DataFrame([x, y])
 #
 # print(df)
 
-s = pd.Series([1, 2, 3, 100], name='t')
-print(s)
+# s = pd.Series([1, 2, 3, 100], name='t')
+# print(s)
