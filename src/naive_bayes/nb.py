3971813cc56f8a21cbb489e5f3e7259fd92b6f29
jaymin.desai <jaymindesai766@gmail.com>
2018-12-02 13:42:57 -0500

    Code Cleanup

--- src/naive_bayes/nb.py
+++ src/naive_bayes/nb.py
@@ -8,167 +8,88 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score
 
 from src.utils.tokenizer import tokenize
 
-import numpy as np
 
-train_data = pd.read_table('../../data/liar/train.tsv')[['label', 'statement']]
-test_data = pd.read_table('../../data/liar/test.tsv')[['label', 'statement']]
+def nb_helper(classifier, n_folds, desc):
+    train_data = pd.read_table('../../data/liar/train.tsv')[['label', 'statement']]
+    test_data = pd.read_table('../../data/liar/test.tsv')[['label', 'statement']]
 
-data = pd.concat([train_data, test_data], ignore_index=True)
+    data = pd.concat([train_data, test_data], ignore_index=True)
 
-X = data['statement']
-y_six = data['label']
-y_two = data['label'].apply(lambda x: 0 if x in ['TRUE', 'mostly-true', 'half-true'] else 1)
+    X = data['statement']
+    y_six = data['label']
+    y_two = data['label'].apply(lambda x: 'TRUE' if x in ['TRUE', 'mostly-true', 'half-true'] else 'FALSE')
 
-print(y_six.value_counts())
-print('')
-print(y_two.value_counts())
-print('')
+    print_vc(y_six)
+    print_vc(y_two)
 
-prep = []
-for x in X.values:
-    prep.append(' '.join([t for t in tokenize(x)]))
+    y_two = y_two.apply(lambda x: 0 if x is 'TRUE' else 1)
 
-X_prep = pd.Series(prep)
+    prep = []
+    for x in X.values:
+        prep.append(' '.join([t for t in tokenize(x)]))
 
-tfidf_vectorizer = TfidfVectorizer(lowercase=False)
-count_vectorizer = CountVectorizer(lowercase=False)
+    X_prep = pd.Series(prep)
 
-# X_tfidf = tfidf_vectorizer.fit_transform(X).toarray()
-# X_bow = count_vectorizer.fit_transform(X).toarray()
+    tfidf_vectorizer = TfidfVectorizer(lowercase=False)
+    count_vectorizer = CountVectorizer(lowercase=False)
 
-# pd.DataFrame(X_tfidf).to_csv(path_or_buf='tfidf.csv')
-# pd.DataFrame(X_bow).to_csv(path_or_buf='bow.csv')
-
-# X_tfidf = np.array(pd.read_csv('tfidf.csv'))
-# X_bow = np.array(pd.read_csv('bow.csv'))
-
-# print('Shape of TFIDF', len(X_tfidf), len(X_tfidf[0]))
-# print('Shape of BOW', len(X_bow), len(X_bow[0]))
-# print('')
-
-skfolds = StratifiedKFold(n_splits=5, random_state=0)
-
-# clf = GaussianNB()
-clf = MultinomialNB()
-
-accuracy_tfidf_six = []
-accuracy_bow_six = []
-recall_tfidf_six = []
-recall_bow_six = []
-precision_tfidf_six = []
-precision_bow_six = []
-
-for train_index, test_index in skfolds.split(X_prep, y_six):
-    cloned_clf = clone(clf)
-
-    X_train = X_prep[train_index]
-    y_train = y_six[train_index]
-
-    X_test = X_prep[test_index]
-    y_test = y_six[test_index]
-
-    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-    X_test_tfidf = tfidf_vectorizer.transform(X_test)
-
-    cloned_clf.fit(X_train_tfidf, y_train)
-
-    y_pred = cloned_clf.predict(X_test_tfidf)
-
-    accuracy_tfidf_six.append(accuracy_score(y_test, y_pred))
-    recall_tfidf_six.append(recall_score(y_test, y_pred, average='macro'))
-    precision_tfidf_six.append(precision_score(y_test, y_pred, average='macro'))
-
-for train_index, test_index in skfolds.split(X_prep, y_six):
-    cloned_clf = clone(clf)
-
-    X_train = X_prep[train_index]
-    y_train = y_six[train_index]
-
-    X_test = X_prep[test_index]
-    y_test = y_six[test_index]
-
-    X_train_bow = count_vectorizer.fit_transform(X_train)
-    X_test_bow = count_vectorizer.transform(X_test)
-
-    cloned_clf.fit(X_train_bow, y_train)
-
-    y_pred = cloned_clf.predict(X_test_bow)
-
-    accuracy_bow_six.append(accuracy_score(y_test, y_pred))
-    recall_bow_six.append(recall_score(y_test, y_pred, average='macro'))
-    precision_bow_six.append(precision_score(y_test, y_pred, average='macro'))
-
-tfidf_six = pd.DataFrame(data={'accuracy': accuracy_tfidf_six,
-                               'recall': recall_tfidf_six,
-                               'precision': precision_tfidf_six})
-
-bow_six = pd.DataFrame(data={'accuracy': accuracy_bow_six,
-                             'recall': recall_bow_six,
-                             'precision': precision_bow_six})
-
-print(tfidf_six)
-print('')
-print(bow_six)
-print('')
-
-accuracy_tfidf_two = []
-accuracy_bow_two = []
-recall_tfidf_two = []
-recall_bow_two = []
-precision_tfidf_two = []
-precision_bow_two = []
-
-for train_index, test_index in skfolds.split(X_prep, y_two):
-    cloned_clf = clone(clf)
-
-    X_train = X_prep[train_index]
-    y_train = y_two[train_index]
-
-    X_test = X_prep[test_index]
-    y_test = y_two[test_index]
-
-    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-    X_test_tfidf = tfidf_vectorizer.transform(X_test)
-
-    cloned_clf.fit(X_train_tfidf, y_train)
-
-    y_pred = cloned_clf.predict(X_test_tfidf)
-
-    accuracy_tfidf_two.append(accuracy_score(y_test, y_pred))
-    recall_tfidf_two.append(recall_score(y_test, y_pred))
-    precision_tfidf_two.append(precision_score(y_test, y_pred))
+    classify(X_prep, y_six, classifier, tfidf_vectorizer, n_folds, 6, desc + '_NB_TFiDF')
+    classify(X_prep, y_six, classifier, count_vectorizer, n_folds, 6, desc + '_NB_BoW')
+    classify(X_prep, y_two, classifier, tfidf_vectorizer, n_folds, 2, desc + '_NB_TFiDF')
+    classify(X_prep, y_two, classifier, count_vectorizer, n_folds, 2, desc + '_NB_BoW')
 
 
-for train_index, test_index in skfolds.split(X_prep, y_two):
-    cloned_clf = clone(clf)
+def classify(X, y, clf, vec, n_folds, n_class, operation='_'):
+    skfolds = StratifiedKFold(n_splits=n_folds, random_state=100)
 
-    X_train = X_prep[train_index]
-    y_train = y_two[train_index]
+    accuracy = []
+    recall = []
+    precision = []
 
-    X_test = X_prep[test_index]
-    y_test = y_two[test_index]
+    for train_index, test_index in skfolds.split(X, y):
+        cloned_clf = clone(clf)
 
-    X_train_bow = count_vectorizer.fit_transform(X_train)
-    X_test_bow = count_vectorizer.transform(X_test)
+        X_train = X[train_index]
+        y_train = y[train_index]
 
-    cloned_clf.fit(X_train_bow, y_train)
+        X_test = X[test_index]
+        y_test = y[test_index]
 
-    y_pred = cloned_clf.predict(X_test_bow)
+        X_train_vec = vec.fit_transform(X_train)
+        X_test_vec = vec.transform(X_test)
 
-    accuracy_bow_two.append(accuracy_score(y_test, y_pred))
-    recall_bow_two.append(recall_score(y_test, y_pred))
-    precision_bow_two.append(precision_score(y_test, y_pred))
+        cloned_clf.fit(X_train_vec, y_train)
+
+        y_pred = cloned_clf.predict(X_test_vec)
+
+        accuracy.append(accuracy_score(y_test, y_pred))
+        recall.append(recall_score(y_test, y_pred, average='binary' if n_class == 2 else 'macro'))
+        precision.append(precision_score(y_test, y_pred, average='binary' if n_class == 2 else 'macro'))
+
+    results = pd.DataFrame(data={'accuracy': accuracy, 'recall': recall, 'precision': precision})
+
+    print('{} Fold CV Results for {} with {} Class Labels:'.format(n_folds, operation, n_class))
+    print('')
+    print(results)
+    print('')
+    print('-----')
+    print('\t')
 
 
-tfidf_two = pd.DataFrame(data={'accuracy': accuracy_tfidf_two,
-                               'recall': recall_tfidf_two,
-                               'precision': precision_tfidf_two})
+def print_vc(y):
+    vc = y.value_counts()
+    n_class = len(vc)
+    print('{} Class Labels:'.format(n_class))
+    print('')
+    for i in vc.index:
+        print('{}: {}'.format(i, vc[i]))
+    print('')
+    print('-----')
+    print('\t')
 
-bow_two = pd.DataFrame(data={'accuracy': accuracy_bow_two,
-                             'recall': recall_bow_two,
-                             'precision': precision_bow_two})
 
-print(tfidf_two)
-print('')
-print(bow_two)
-print('')
+if __name__ == '__main__':
+    print('\t')
+    folds = 5
+    # nb_helper(GaussianNB(), folds, desc='Gaussian')
+    nb_helper(MultinomialNB(), folds, desc='Multinomial')
